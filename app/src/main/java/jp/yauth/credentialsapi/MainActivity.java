9ee231ab4aa4122a375601734f514ac62289c8ca
nov <nov@matake.jp>
2015-12-25 17:36:31 +0900

    init

---
+++ app/src/main/java/jp/yauth/credentialsapi/MainActivity.java
@@ -0,0 +1,144 @@
+package jp.yauth.credentialsapi;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialRequest;
+import com.google.android.gms.auth.api.credentials.CredentialRequestResult;
+import com.google.android.gms.auth.api.credentials.IdentityProviders;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    final int RC_READ = 1;
+    GoogleApiClient mCredentialsClient;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCredentialsClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAuthentication();
+            }
+        });
+    }
+
+    private void startAuthentication() {
+        Log.d(TAG, "startAuthentication");
+
+
+        CredentialRequest mCredentialRequest = new CredentialRequest.Builder()
+                .setPasswordLoginSupported(true)
+                .setAccountTypes(IdentityProviders.GOOGLE, IdentityProviders.FACEBOOK)
+                .build();
+
+        Auth.CredentialsApi.request(mCredentialsClient, mCredentialRequest).setResultCallback(
+                new ResultCallback<CredentialRequestResult>() {
+                    @Override
+                    public void onResult(CredentialRequestResult credentialRequestResult) {
+                        Log.d(TAG, "onResult");
+                        if (credentialRequestResult.getStatus().isSuccess()) {
+                            onCredentialRetrieved(credentialRequestResult.getCredential());
+                        } else {
+                            resolveResult(credentialRequestResult.getStatus());
+                        }
+                    }
+                });
+    }
+
+    private void onCredentialRetrieved(Credential credential) {
+        String accountType = credential.getAccountType();
+        if (accountType == null) {
+            // Sign the user in with information from the Credential.
+            Log.d(TAG, "onCredentialRetrieved:password");
+        } else if (accountType.equals(IdentityProviders.GOOGLE)) {
+            // The user has previously signed in with Google Sign-In. Silently
+            // sign in the user with the same ID.
+            // See https://developers.google.com/identity/sign-in/android/
+            GoogleSignInOptions gso =
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestEmail()
+                            .build();
+            GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .enableAutoManage(this, this)
+                    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                    .setAccountName(credential.getId())
+                    .build();
+            OptionalPendingResult<GoogleSignInResult> opr =
+                    Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+            // ...
+        }
+    }
+
+    private void resolveResult(Status status) {
+        if (status.getStatusCode() == CommonStatusCodes.RESOLUTION_REQUIRED) {
+            // Prompt the user to choose a saved credential; do not show the hint
+            // selector.
+            try {
+                status.startResolutionForResult(this, RC_READ);
+            } catch (IntentSender.SendIntentException e) {
+                Log.e(TAG, "STATUS: Failed to send resolution.", e);
+            }
+        } else {
+            // The user must create an account or sign in manually.
+            Log.e(TAG, "STATUS: Unsuccessful credential request.");
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(TAG, "onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_READ) {
+            if (resultCode == RESULT_OK) {
+                Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+                onCredentialRetrieved(credential);
+            } else {
+                Log.e(TAG, "Credential Read: NOT OK");
+                Toast.makeText(this, "Credential Read Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+}
